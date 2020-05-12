a8010ab439aca64da7ed6bb0b7cb9dc5e56ceca1
Sandeep Banisetti <banises@amazon.com>
2020-05-12 14:00:57 -0700

    Initial commit

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth } from 'aws-amplify';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+  user = 'Sandeep';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  onLogOut() {
+    Auth.signOut()
+      .then(data => {
+        console.log(data);
+        console.log("You are successfully logged out");
+        this.router.navigate(["/login"]);
+      })
+      .catch(err => console.log(err));
+  }
+}
